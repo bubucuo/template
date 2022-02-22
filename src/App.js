085d93a38c81444745bb85b71bf6e1f722fdda0e
bubucuo <276185876@qq.com>
2022-02-22 16:25:03 +0800

    init

--- src/App.js
+++ src/App.js
@@ -1,33 +1,7 @@
-import { useLayoutEffect } from "react";
-import { CanvasContext } from "./Context";
-import Cmps from "./layouts/Cmps";
-import Content from "./layouts/Content";
-import Edit from "./layouts/Edit";
-import { useForceUpdate } from "./layouts/hooks";
-import { useCanvas } from "./store/globalCanvas";
-import styles from "./App.less";
-
-export default function App() {
-  const forceUpdate = useForceUpdate();
-
-  // 所有组件
-  const globalCanvas = useCanvas();
-
-  useLayoutEffect(() => {
-    const unsubscribe = globalCanvas.subscribe(() => {
-      forceUpdate();
-    });
-    return () => {
-      unsubscribe();
-    };
-  }, [globalCanvas, forceUpdate]);
+export default function App(props) {
   return (
-    <div id="app" className={styles.main}>
-      <CanvasContext.Provider value={globalCanvas}>
-        <Cmps />
-        <Content />
-        <Edit />
-      </CanvasContext.Provider>
+    <div>
+      <h3>App</h3>
     </div>
   );
 }
